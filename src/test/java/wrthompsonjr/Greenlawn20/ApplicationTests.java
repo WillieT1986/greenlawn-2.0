54fc5aa48eecffd6bdb35fe98443ba1cb1494fe1
Willie T <wrthompsonjr86@gmail.com>
2021-12-15 18:52:25 -0500

    Initial Commit and Setup.

---
+++ src/test/java/wrthompsonjr/Greenlawn20/ApplicationTests.java
@@ -0,0 +1,13 @@
+package wrthompsonjr.Greenlawn20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
