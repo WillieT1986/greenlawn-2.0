fce34542df40d3cc9e62d8ccc14a9c362d8c6710
Willie T <wrthompsonjr86@gmail.com>
2021-12-20 15:28:34 -0500

    Iteration 2 (#4)
    * Formed a Many To One Relationship between Gravesite and Cemetery Section. Updated Tests. Ran Tests - Tests Passed.
    
    * Formed a One To Many Relationship between Cemetery Section and Grave Site. Added Entities & Id's to both GraveSite and CemeterySection.
    
    * Form Many To Many Relationships between GraveSite & CemeteryTag. Checked Tests For Errors - No Errors Present. Ran Tests - Tests Passed.
    
    * Add .circleci/config.yml

--- src/main/java/wrthompsonjr/Greenlawn20/models/GraveSite.java
+++ src/main/java/wrthompsonjr/Greenlawn20/models/GraveSite.java
@@ -1,6 +1,28 @@
 package wrthompsonjr.Greenlawn20.models;
 
+import net.minidev.json.annotate.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.HashSet;
+
+import static java.util.Arrays.asList;
+
+@Entity
 public class GraveSite {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @JsonIgnore
+    @ManyToOne
+    private CemeterySection cemeterySection;
+
+    @JsonIgnore
+    @ManyToMany
+    private Collection<CemeteryTag> cemeteryTags;
+
 
     private String name;
     private String dateOfBirth;
@@ -8,12 +30,21 @@ public class GraveSite {
     private String status;
     private String obituary;
 
-    public GraveSite(String name, String dateOfBirth, String dateOfDeath, String status, String obituary) {
+    public GraveSite() {
+    }
+
+    public GraveSite(String name, String dateOfBirth, String dateOfDeath, String status, String obituary, CemeterySection cemeterySection, CemeteryTag... cemeteryTags) {
         this.name = name;
         this.dateOfBirth = dateOfBirth;
         this.dateOfDeath = dateOfDeath;
         this.status = status;
         this.obituary = obituary;
+        this.cemeterySection = cemeterySection;
+        this.cemeteryTags = new HashSet<>(asList(cemeteryTags));
+    }
+
+    public Collection<CemeteryTag> getCemeteryTags() {
+        return cemeteryTags;
     }
 
     public String getName() {
@@ -34,5 +65,25 @@ public class GraveSite {
 
     public String getObituary() {
         return obituary;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public int hashCode() {
+        return ((Long) id).hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        return id == ((GraveSite) obj).id;
     }
 }
