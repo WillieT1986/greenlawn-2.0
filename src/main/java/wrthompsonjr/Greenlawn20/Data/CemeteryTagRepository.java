d04f3cbf24068682733484e7779cc705c5ca70c5
Willie T <wrthompsonjr86@gmail.com>
2021-12-21 14:52:19 -0500

    Through Unit & Mock Testing, created Controller & Repository for CemeteryTag. Tested for Adding & Returning an individual CemeteryTag to model, Tests passed.

---
+++ src/main/java/wrthompsonjr/Greenlawn20/Data/CemeteryTagRepository.java
@@ -0,0 +1,10 @@
+package wrthompsonjr.Greenlawn20.Data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import wrthompsonjr.Greenlawn20.models.CemeteryTag;
+
+@Repository
+public interface CemeteryTagRepository extends JpaRepository<CemeteryTag, Long> {
+
+}
---
+++ src/test/java/wrthompsonjr/Greenlawn20/controllers/CemeteryTagControllerTest.java
@@ -0,0 +1,50 @@
+package wrthompsonjr.Greenlawn20.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+import wrthompsonjr.Greenlawn20.Data.CemeteryTagRepository;
+import wrthompsonjr.Greenlawn20.models.CemeteryTag;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class CemeteryTagControllerTest {
+
+    @InjectMocks
+    CemeteryTagController underTest;
+
+    @Mock
+    private CemeteryTagRepository cemeteryTagRepo;
+
+    @Mock
+    private CemeteryTag cemeteryTag1;
+
+    @Mock
+    Model model;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void shouldAddAnIndividualCemeteryTagToModel() {
+        long cemeteryTagId = 3L;
+        when(cemeteryTagRepo.getById(cemeteryTagId)).thenReturn(cemeteryTag1);
+        underTest.findOneCemeteryTag(cemeteryTagId, model);
+        verify(model).addAttribute("CemeteryTag", cemeteryTag1);
+    }
+
+    @Test
+    public void shouldReturnAnIndividualCemeteryTagToModel() {
+        String template = underTest.findOneCemeteryTag(3L, model);
+        assertThat(template, is("CemeteryTag"));
+    }
+
+}
