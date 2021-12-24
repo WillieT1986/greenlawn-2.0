39e43122cb86962a7b60e94b49a1242ffa6ea274
Willie T <wrthompsonjr86@gmail.com>
2021-12-24 15:25:28 -0500

    Through Testing, started REST Controller for GraveSite. Retrieve Any Gravesite, Find All Gravesites, Get One Gravesite, Return a Cannot Find Exception, and return a List of CemeterySections was tested out so far. All Tests Pass.

---
+++ src/main/java/wrthompsonjr/Greenlawn20/controllers/GraveSiteRestController.java
@@ -0,0 +1,60 @@
+package wrthompsonjr.Greenlawn20.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import wrthompsonjr.Greenlawn20.data.repository.CemeterySectionRepository;
+import wrthompsonjr.Greenlawn20.data.repository.GraveSiteRepository;
+import wrthompsonjr.Greenlawn20.models.GraveSite;
+
+import javax.annotation.Resource;
+
+@RestController
+public class GraveSiteRestController {
+
+    @SuppressWarnings("unused")
+    private Logger log = LoggerFactory.getLogger(GraveSiteRestController.class);
+
+    @Resource
+    private GraveSiteRepository graveSiteRepo;
+
+    @Resource
+    private CemeterySectionRepository cemeterySectionRepo;
+
+    public String findGraveSites(Model model) {
+        model.addAttribute("graveSites", graveSiteRepo.findAll());
+        return "graveSites";
+    }
+
+    @RequestMapping("/gravesite/{id}")
+    public GraveSite findGraveSite(@PathVariable(name = "id") long id) {
+        if (graveSiteRepo.getById(id) == null) {
+            throw new CannotFindException("Gravesite Does Not Exist.");
+        }
+        return graveSiteRepo.getById(id);
+    }
+
+    public String findCemeterySections(Model model) {
+        model.addAttribute("cemeterySections", cemeterySectionRepo.findAll());
+        return "cemeterySections";
+    }
+
+    @SuppressWarnings("serial")
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public class CannotFindException extends RuntimeException {
+        private String message;
+
+        public String getMessage() {
+            return message;
+        }
+
+        CannotFindException(String errorMessage) {
+            this.message = errorMessage;
+        }
+    }
+}
---
+++ src/test/java/wrthompsonjr/Greenlawn20/controllers/GraveSiteRestControllerTest.java
@@ -0,0 +1,103 @@
+package wrthompsonjr.Greenlawn20.controllers;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+import wrthompsonjr.Greenlawn20.controllers.GraveSiteRestController.CannotFindException;
+import wrthompsonjr.Greenlawn20.data.repository.CemeterySectionRepository;
+import wrthompsonjr.Greenlawn20.data.repository.CemeteryTagRepository;
+import wrthompsonjr.Greenlawn20.data.repository.GraveSiteRepository;
+import wrthompsonjr.Greenlawn20.models.CemeterySection;
+import wrthompsonjr.Greenlawn20.models.CemeteryTag;
+import wrthompsonjr.Greenlawn20.models.GraveSite;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.any;
+import static org.hamcrest.Matchers.contains;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.util.AssertionErrors.assertTrue;
+
+public class GraveSiteRestControllerTest {
+
+    @InjectMocks
+    private GraveSiteRestController underTest;
+
+    @Mock
+    private GraveSite graveSite;
+
+    @Mock
+    private CemeterySection cemeterySection;
+
+//    @Mock
+//    private CemeteryTag cemeteryTag;
+
+    @Mock
+    private GraveSiteRepository graveSiteRepo;
+
+    @Mock
+    private CemeterySectionRepository cemeterySectionRepo;
+
+//    @Mock
+//    private CemeteryTagRepository cemeteryTagRepos;
+
+//    @Mock
+//    private Collection<GraveSite> graveSites;
+
+    @Mock
+    Model model;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void shouldRetrieveAnyGraveSite() throws Exception {
+        when(graveSiteRepo.findAll()).thenReturn(Collections.singletonList(graveSite));
+        String result = underTest.findGraveSites(model);
+        assertTrue(result, contains(any(GraveSite.class)) != null);
+    }
+
+    @Test
+    public void shouldFindAllGraveSitesFromDatabase() throws Exception {
+        when(graveSiteRepo.findAll()).thenReturn(Collections.singletonList(graveSite));
+        String result = underTest.findGraveSites(model);
+        assertTrue(result, contains(graveSite) != null);
+    }
+
+    @Test
+    public void shouldGetOneGraveSiteFromDatabase() {
+        when(graveSiteRepo.getById(150000L)).thenReturn(graveSite);
+        GraveSite result = underTest.findGraveSite(150000L);
+        assertThat(result, is(graveSite));
+    }
+
+    @Test
+    public void shouldReturnCannotFindExceptionForGraveSiteId() throws Exception {
+        CannotFindException thrown = assertThrows(
+                CannotFindException.class,
+                () -> {
+                    long invalidGraveSiteId = 54L;
+                    underTest.findGraveSite(invalidGraveSiteId);
+                });
+        assertEquals("Gravesite Does Not Exist.", thrown.getMessage());
+    }
+
+    @Test
+    public void shouldReturnAListOfCemeterySections() {
+        when(cemeterySectionRepo.findAll()).thenReturn(Collections.singletonList(cemeterySection));
+        String result = underTest.findCemeterySections(model);
+        assertTrue(result, contains(any(CemeterySection.class)) != null);
+    }
+
+}
